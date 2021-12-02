febb5fd95c4ffa4e1f8732bea1de8de8d4337775
MARIANA INOUE <marinoue@gmail.com>
2021-12-02 17:05:37 -0300

    patch validator

--- src/controllers/vaccineController.js
+++ src/controllers/vaccineController.js
@@ -1,4 +1,9 @@
 const Vaccine = require('../models/Vaccine')
+const { body, param, validationResult } = require('express-validator')
+const ErrorResponse = require('./error/error-response')
+const NotFoundError = require('./error/error-not-found')
+const BadRequestError = require('./error/bad-request-error')
+const ErrorService = require('./error/error')
 
 const getAllVaccine = async(req, res)=> {
      const {vaccineAll} = req.params     
@@ -18,6 +23,7 @@ const getAllVaccine = async(req, res)=> {
 
 
 const getVaccine = async(req, res)=> {
+
     const requiredId = req.params.id
     try{
         const vaccine = await Vaccine.findOne( { where: {id: requiredId} })
@@ -32,30 +38,57 @@ const getVaccine = async(req, res)=> {
 }
 const createVaccine = async(req, res)=> {
     const { name, expected_date, vaccinated } = req.body
+    const error = validationResult(req)
+
     try{
-        const vaccine = await Vaccine.create({ name, expected_date, vaccinated })
-        console.log(` Cadasto de vacina criado`)
-        res.status(201).send(vaccine)
+        if(!error.isEmpty()){
+            ErrorResponse(res, {message:error.array()},400, 'Bad Request')
+        }else{
+            const vaccine = await Vaccine.create({ name, expected_date, vaccinated })
+            console.log(` Cadasto de vacina criado`)
+            res.status(201).send(vaccine)
+        }
+        
     }catch(error){
-        res.status(500).send({ message: error.message})
+        ErrorResponse(res, error)
 
     }
     
 }
 const updateVaccine = async(req, res)=> {
-    const { id } = req.params
-    const { vaccinated } = req.body
+    const idVaccinne = req.params.id
+    const vaccinated = req.body.vaccinated
+    const error =validationResult(req)
+        
     try{ 
-        const rowsUpdated = await Vaccine.update( { vaccinated } , { where: { id } })
-        if( rowsUpdated && rowsUpdated>0 ){
-            res.status(200).send({ message: `${rowsUpdated[0]} vacina atualizada.`})
-        }else{
-            res.status(404).send( {message: `Vacina com id ${id} não encotrada para atualizar`})
-        }
+        if(!error.isEmpty()){
+            ErrorResponse(res ,{message: error.array()}, 400, 'Bad request')}
+        else{
+            const rowsUpdated = await Vaccine.update( { vaccinated } , { where: { id:idVaccinne } })
+            if( rowsUpdated && rowsUpdated>0 ){
+                res.status(200).send({ message: `${rowsUpdated[0]} vacina atualizada.`})
+                } 
+            else{
+                res.status(404).send({ message: `Vaicna  com id ${idVaccinne} não encontrado para atualizar informação de favorito` })
+                }
+            }
     }catch(error){
-        res.status(500).send({ message: error.message })    
+        ErrorResponse(res,{ message: `Medico com id ${idVaccinne} não encontrado para atualizar informação de favorito` })    
     }
-}
+}   
+       
+            
+        
+      
+   
+           
+            
+            
+        
+            
+        
+
+
 
               
 
--- src/routes/vaccineRoutes.js
+++ src/routes/vaccineRoutes.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const { body, param, validationResult } = require('express-validator')
 const controller = require('../controllers/vaccineController')
 
-router.post('/',controller.createVaccine)
 
 router.get('/', controller.getAllVaccine)
 
 router.get('/:id', controller.getVaccine)
 
-router.patch('/:id', controller.updateVaccine)
+router.post('/',[
+
+    body('name').notEmpty().withMessage('This field is required'),
+    body('expected_date').isLength({ min: 10}).withMessage('This field is required day/month/year'),
+],controller.createVaccine )
+
+router.patch('/:id', [
+    param('id')
+        .isNumeric()
+        .withMessage('The parameter ID must be an interger')
+
+], controller.updateVaccine)
     
 module.exports = router
